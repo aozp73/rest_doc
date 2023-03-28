fdebaa1facd5f89f4be5054140a4adde260f6129
aozp73 <aozp73@naver.com>
2023-03-29 08:49:02 +0900

    initial commit

---
+++ src/main/java/shop/mtcoding/rest_doc/RestDocApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.rest_doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/rest_doc/RestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.rest_doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
